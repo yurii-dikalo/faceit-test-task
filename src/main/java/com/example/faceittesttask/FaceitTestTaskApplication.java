ef710d856a70e3cd089838e5aff3fd5bef673910
Yurii Dikalo <dikalotel@gmail.com>
2023-05-02 15:49:00 +0300

    initial commit

---
+++ src/main/java/com/example/faceittesttask/FaceitTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.faceittesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceitTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceitTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/faceittesttask/FaceitTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.faceittesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceitTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
