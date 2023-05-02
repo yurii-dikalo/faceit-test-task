ce5cb7a2b961be74a8851bd6e9926897cdd0d241
Yurii Dikalo <dikalotel@gmail.com>
2023-05-02 16:06:23 +0300

    added checkstyle

--- src/main/java/com/example/faceittesttask/FaceitTestTaskApplication.java
+++ src/main/java/com/example/faceittesttask/FaceitTestTaskApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FaceitTestTaskApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FaceitTestTaskApplication.class, args);
     }
-
 }
